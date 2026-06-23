f8dfd0ea0a67c0a43ad76ef67096e81eb458a286
Yannick Olinga <yannickolinga213@gmail.com>
2026-06-23 23:00:26 +0200

    fix: align Expo SDK 54 deps and EAS build config

--- pierrine/app.config.js
+++ pierrine/app.config.js
@@ -1,18 +1,36 @@
-const { expo } = require("./app.json");
 const { getApiBaseUrl } = require("./scripts/get-local-ip");
 
-// Ne pas écraser si déjà défini dans .env
-const apiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL ?? getApiBaseUrl();
+const PRODUCTION_API_URL = "https://perinea.osc-fr1.scalingo.io";
 
-// Disponible dans le bundle client (axios, fetch, etc.)
-process.env.EXPO_PUBLIC_API_BASE_URL = apiBaseUrl;
+function resolveApiBaseUrl() {
+  if (process.env.EXPO_PUBLIC_API_BASE_URL) {
+    return process.env.EXPO_PUBLIC_API_BASE_URL.replace(/\/$/, "");
+  }
 
-module.exports = {
-  expo: {
-    ...expo,
+  if (process.env.EAS_BUILD_PROFILE === "production") {
+    return PRODUCTION_API_URL;
+  }
+
+  try {
+    return getApiBaseUrl();
+  } catch {
+    return PRODUCTION_API_URL;
+  }
+}
+
+/** @param {{ config: import("@expo/config-types").ExpoConfig }} ctx */
+module.exports = ({ config }) => {
+  const apiBaseUrl = resolveApiBaseUrl();
+  process.env.EXPO_PUBLIC_API_BASE_URL = apiBaseUrl;
+
+  return {
+    ...config,
     extra: {
-      ...expo.extra,
+      ...config.extra,
       apiBaseUrl,
+      eas: {
+        projectId: "a53b1077-052e-4d85-9765-f47dfe04781d",
+      },
     },
-  },
+  };
 };
